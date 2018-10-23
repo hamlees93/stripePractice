2656b51383d43ef9d05ff7619aafe1ca228d2664
Hamish Lees <hamishlees6@gmail.com>
2018-10-23 14:40:09 +1100

    test app with api and stripe

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,29 @@
+class PagesController < ApplicationController
+    before_action :authenticate_user!
+    
+    def home
+    end
+
+    def process_payment
+        
+        # Amount in cents
+        @amount = 1500
+
+       customer = Stripe::Customer.retrieve(current_user.customer_id)
+       customer.source = params[:stripeToken]
+       customer.save
+
+        charge = Stripe::Charge.create(
+            :customer    => current_user.customer_id,
+            :amount      => @amount,
+            :description => 'Rails Stripe customer',
+            :currency    => 'usd'
+        )
+
+        redirect_to "/"
+
+        rescue Stripe::CardError => e
+        flash[:error] = e.message
+        redirect_to "/"
+    end
+end
